56ab8f34107564edd60cc73dcae71eb0d0d3e87a
Ruybic Cookies <89454325+Ruybic@users.noreply.github.com>
2026-02-28 06:36:10 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App' // Vite is smart enough to find App.jsx even if you don't type the extension here!
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)

6c3ddeed67a81a5d7a9ea62bd69f4b55d552bff4
Ruybic Cookies <89454325+Ruybic@users.noreply.github.com>
2026-02-28 03:10:56 +0300

    Update fetch-daily.js

--- fetch-daily.js
+++ fetch-daily.js
@@ -57,6 +57,8 @@ async function main(){
 
     const batchPromises = batch.map(async (userId) => {
       try {
+        // ("Recent scores" limit) | Ruybic: This line has the amount of recent scores pulled grom the official osu! v2 api.
+        // ("Recent scores" limit) | Ruybic: As Limit set to 50, because it is very unlikely a player submits over >50 scores within 24hr period.
         const res = await fetch(`https://osu.ppy.sh/api/v2/users/${userId}/scores/recent?limit=50&include_fails=0`,{headers});
         const data = await res.json();
         if (!Array.isArray(data)) return [];
